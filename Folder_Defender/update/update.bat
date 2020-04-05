a1328996727e1a833a3eb3b8dc0f5ef0634196f2
Kanta S <63178234+kanta0218@users.noreply.github.com>
2020-04-05 13:52:18 +0900

    Delete update.bat

--- Folder_Defender/update/update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo [Folder Defender for Windows OS] Update Checker
-echo.
-echo ================================================
-echo Download the version file.
-echo Download Directory: %~dp0
-bitsadmin /TRANSFER DL_VER_FILE http://kirito16.ihostfull.com/server/applications/Folder_Defender/latest.version.txt %~dp0latest.version.txt
-echo Check the version.
-pause
